c2dc474552c39c34689f976aa0464707f1b0deb0
Andrei Solodovnikov <VoultBoy@yandex.ru>
2023-05-03 11:15:37 +0300

    Update tb_cybercobra.v

--- Labs/4. Primitive programmable device/tb_cybercobra.v
+++ Labs/4. Primitive programmable device/tb_cybercobra.v
@@ -5,14 +5,14 @@ module tb_CYBERcobra();
     CYBERcobra dut(
     .clk_i(clk),
     .rst_i(rstn),
-    .sw_i (INr ),
+    .sw_i (sw_i ),
     .out_o(OUT)
     );
     
     wire [31:0] OUT;
     reg clk;
     reg rstn;
-    reg [15:0] INr;
+    reg [15:0] sw_i;
 
     initial clk <= 0;
     always #5 clk = ~clk;
@@ -22,9 +22,9 @@ module tb_CYBERcobra();
    
     #10;
     rstn = 1'b0;
-    INr = 15'b100001000; //значение, до которого считает счетчик
+    sw_i = 16'b100001000; //значение, до которого считает счетчик
     //#260;
-    //INr = 15'b0;
+    //sw_i = 15'b0;
     #10000;
     $display("\n ТЕСТ ОКОНЧЕН \n Смотри внутренние сигналы кобры на времянке \n");
     $finish;
