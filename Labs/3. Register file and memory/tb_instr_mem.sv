04aeab30a34ad1436e98d48028394bd4347eaa15
Nekkit6 <bulavin20011001@gmail.com>
2023-08-04 16:08:12 +0300

    Изменение тб памяти инст и даты под новую методич

--- Labs/3. Register file and memory/tb_instr_mem.sv
+++ Labs/3. Register file and memory/tb_instr_mem.sv
@@ -15,8 +15,8 @@ parameter TIME_OPERATION  = 100;
     );
     
     instr_mem DUT (
-    .addr(A),
-    .read_data(RD)
+    .addr_i(A),
+    .read_data_o(RD)
     );
     
     integer i, err_count = 0;
@@ -24,16 +24,23 @@ parameter TIME_OPERATION  = 100;
     assign A = i;
     
     initial begin
-        $display( "\nStart test: \n\n========================\nНАЖМИ НА КНОПКУ 'Run All'\n========================\n"); $stop();
+        $display( "\nStart test: \n\n==========================\nCLICK THE BUTTON 'Run All'\n==========================\n"); $stop();
         for (i = 0; i < ADDR_SIZE; i = i + 1) begin
             #TIME_OPERATION;
             if ( RD !== RDref) begin
                 $display("По адресу %d получены данные %h, а должно быть %h", A, RD, RDref);
                 err_count = err_count + 1;
             end
-        end    
+        end
+        for (i = 1021; i < 1024; i = i + 1) begin
+            #TIME_OPERATION;
+            if ( RD !== 32'b0) begin
+                $display("ERROR! Addr = %d", A, " %h != 0", RD);
+                err_count = err_count + 1;
+            end
+        end
+        $display("Number of errors: %d", err_count);
         if( !err_count )  $display("\n instr_mem SUCCESS!!!\n");
-        else $display("\nТест завершен с ошибками\n");
         $finish();
     end
     
